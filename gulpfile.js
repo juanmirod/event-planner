9d66c8aefe2069c1069fd2d915f444a2b7c220ba
Juanmi Rodriguez Cerón <juanmi.rodriguez.ceron@gmail.com>
2016-08-14 00:16:02 +0200

    Documented gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -9,10 +9,18 @@ var autoprefix = new LessAutoprefix({ browsers: ['last 2 versions'] });
 var mainBowerFiles = require('main-bower-files');
 var jasmine = require('gulp-jasmine-phantom');
 
+
+
+/*
+  Reloads the server
+  */
 gulp.task('reload', function(){
     browserSync.reload();
 });
 
+/*
+  Copies bower main files to the public folder
+  */
 gulp.task('bower', function() {
  gulp.src(mainBowerFiles(), {
       base: 'bower_components'
@@ -21,27 +29,49 @@ gulp.task('bower', function() {
   
 });
 
+//////////////////////////// STYLES ///////////////////////////////////////////////
+
+/*
+  Copy our custom variables.less file to the bootstrap folder in public
+  */
 gulp.task('bootstrap:copyLess', ['bower'], function() {
   gulp.src('less/variables.less')
     .pipe(gulp.dest('public/lib/bootstrap/less'));
 });
 
+/*
+  Compile Bootstrap using our custom variables
+  */
 gulp.task('bootstrap:compile', ['bootstrap:copyLess'], function() {
   gulp.src('public/lib/bootstrap/less/bootstrap.less')
     .pipe(less())
     .pipe(gulp.dest('public/lib/bootstrap/dist/css'));
 });
 
+/*
+  Compile any other extra less file in the less folder
+  */
 gulp.task('customStyles', function(){
 
-  gulp.src('less/**/*.less')
+  gulp.src('less/**/!(variables).less')
     .pipe(less(['autoprefix']))
     .pipe(gulp.dest('./public/css'));
 
 })
 
+/*
+  Run the less compilation, both bootstrap and application custom files 
+  */
 gulp.task('styles', ['bootstrap:compile', 'customStyles']);
 
+
+////////////////////////////////////// JS //////////////////////////////////////////////
+
+/*
+  This task builds the two main js files, one for third party libraries (vendor.js)
+  and one for the app (app.js). It also copies all html templates to the public folder to 
+  be able to serve them.
+  */
 gulp.task('buildjs', function() {
 
   gulp.src('js/**/!(*_test).js')
@@ -63,6 +93,9 @@ gulp.task('buildjs', function() {
 
 });
 
+/*
+  Run the tests suite once
+  */
 gulp.task('tests', ['buildjs'], function () {
   
   gulp.src([
@@ -77,12 +110,21 @@ gulp.task('tests', ['buildjs'], function () {
 
 });
 
+/*
+  Watches for js changes and run the tests suite
+  */
 gulp.task('testrunner', ['tests'], function() {
 
   gulp.watch('js/**/*.js', ['tests']);
 
-})
+});
 
+///////////////////////////// LIVE RELOADING ////////////////////////////////////////
+
+/* 
+  The default task runs browserSync server and watches for changes 
+  in css, js or html to reload the browser.
+  */
 gulp.task('default', ['styles', 'buildjs'], function() {
 
   browserSync.init({
